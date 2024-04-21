e5ea76e0e60ad7ebeeb889319c414e3bee1eca5e
Cristian G <crttngr@gmail.com>
2024-04-21 15:45:23 +0200

    eliminar código sin uso

--- src/tiendaVideojuegos/Tienda.java
+++ src/tiendaVideojuegos/Tienda.java
@@ -1,16 +1,9 @@
 package tiendaVideojuegos;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
 
 public class Tienda {
-	
-	private static ArrayList<String[]> archivos = new ArrayList<>();
 	
 	public static void crearArchivoTienda() {
 		File archivo;
@@ -25,34 +18,6 @@ public class Tienda {
 			e.printStackTrace(System.out);
 		}
 	}
-
-	public static void cargarDatosTienda() {
-		archivos.clear();
-		try {
-			BufferedReader br = new BufferedReader(new FileReader("tienda.txt"));
-			String linea;
-			while ((linea = br.readLine()) != null) {
-				String[] compraData = linea.split(", ");
-				archivos.add(compraData);
-			}
-			br.close();
-		} catch (IOException e) {
-			System.out.println("Error al cargar los archivos de tienda.");
-		}
-	}
-
-	public static void guardarCompraEnArchivo() {
-		try {
-			BufferedWriter bw = new BufferedWriter(new FileWriter("tienda.txt"));
-			for (String[] archivo : archivos) {
-				bw.write(String.join(", ", archivo));
-				bw.newLine();
-			}
-			bw.close();
-		} catch (IOException e) {
-			System.out.println("Error al guardar las compras.");
-		}
-	}
 	
 	
 	public static void obtenerBalanceTienda() {
@@ -64,7 +29,7 @@ public class Tienda {
 		if (balanceActual > 0) {
 			System.out.println("El balance actual de la tienda es de: " + estilosConsola.ANSI_GREEN + balanceActual + estilosConsola.ANSI_WHITE + "€");
 		} else {
-			System.out.println("El balance actual de la tienda es de: " + estilosConsola.ANSI_RED + balanceActual + estilosConsola.ANSI_WHITE + "€");
+			System.out.println("El balance actual de la tienda es de: " + estilosConsola.ANSI_RED + balanceActual + estilosConsola.ANSI_WHITE + " €");
 		}
 		
 	}
